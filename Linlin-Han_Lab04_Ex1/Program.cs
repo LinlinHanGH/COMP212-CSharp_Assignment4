ad10b93d669055777239f03e0dabdcc8421fb330
Linlin <hanlinlin@live.com>
2018-07-14 15:56:36 -0400

    create test code in ex1 (ex1 finished)

--- Linlin-Han_Lab04_Ex1/Program.cs
+++ Linlin-Han_Lab04_Ex1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinkedListLibrary;
 
 namespace Linlin_Han_Lab04_Ex1
 {
@@ -10,6 +11,32 @@ namespace Linlin_Han_Lab04_Ex1
     {
         static void Main(string[] args)
         {
+            var list = new List();// create List container
+
+            // create data to store in List
+            double num1 = 1;
+            double num2 = 2;
+            double num3 = 3;
+
+            list.InsertAtFront(num1);
+            list.InsertAtFront(num2);
+            list.InsertAtFront(num3);
+
+            list.Display();
+
+            Console.WriteLine("Search( ) method: search for 1");
+            list.Search(num1);
+            Console.WriteLine("Search( ): search for 4");
+            list.Search(4);
+            Console.WriteLine();
+
+            Console.WriteLine("Count( ) method");
+            Console.WriteLine("The number of list items is " + list.Count());
+            Console.WriteLine();
+
+            Console.WriteLine("Sum() method");
+            Console.WriteLine("The sum of list items is " + list.Sum());
+
         }
     }
 }
