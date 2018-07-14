9edfefb981d5ba48444f038ac74c19429dbcddbb
Linlin <hanlinlin@live.com>
2018-07-14 16:36:56 -0400

    add stack reference to ex2

--- Linlin_Han_Lab04_Ex02/Program.cs
+++ Linlin_Han_Lab04_Ex02/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LinkedListLibrary;
+using StackInheritanceLibrary;
+
 
 
 namespace Linlin_Han_Lab04_Ex02
@@ -12,6 +14,17 @@ namespace Linlin_Han_Lab04_Ex02
     {
         static void Main(string[] args)
         {
+            var stack = new StackInheritance();
+            // create data to store in List
+            double num1 = 1;
+            double num2 = 2;
+            double num3 = 3;
+
+            stack.Push(num1);
+            stack.Push(num2);
+            stack.Push(num3);
+
+            stack.Display();
         }
     }
 }
--- StackInheritanceLibrary/StackInheritance.cs
+++ StackInheritanceLibrary/StackInheritance.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LinkedListLibrary;
 
+
 namespace StackInheritanceLibrary
 {
     // class StackInheritance inherits class List's capabilities
